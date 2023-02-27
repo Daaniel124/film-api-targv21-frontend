b6791e44f59adbe37e1a8306a40e00c8361c0a1b
daaniel124 <daaniel124@gmail.com>
2023-02-27 18:11:36 +0200

    Add button Delete

--- src/components/filmList.vue
+++ src/components/filmList.vue
@@ -3,25 +3,47 @@
         <tr>
             <th>Title</th>
             <th></th>
+            <th></th>
         </tr>
         <tr v-for="film in films" :key="film.id">
             <td>{{ film.title }}</td>
             <td>
-                <button id="show-modal" @click="filmDetailId = film.id">Show Details</button>
+                <button id="show-details-modal" @click="filmDetailId = film.id">Show Details</button>
+            </td>
+            <td>
+                <button id="show-delete-modal" @click="filmDeleteId = film.id">Delete</button>
             </td>
         </tr>
         
     </table>
     <Teleport to="body">
-    <!-- use the modal component, pass in the prop -->
-    <modal :show="filmDetailId !==0" @close="filmDetailId = 0">
-      <template #header>{{currentFilm.title}}
-      </template>
-      <template #body>
-        <filmView :film="currentFilm"></filmView>
-      </template>
-    </modal>
-  </Teleport>
+        <!-- use the modal component, pass in the prop -->
+        <modal :show="filmDetailId !==0" @close="filmDetailId = 0">
+        <template #header>{{currentFilm.title}}</template>
+        <template #body>
+            <filmView :film="currentFilm"></filmView>
+        </template>
+        </modal>
+    </Teleport>
+    <Teleport to="body">
+        <!-- use the modal component, pass in the prop -->
+        <modal :show="filmDeleteId !==0" @close="filmDeleteId = 0">
+        <template #header>Delete {{currentFilm.title}} ?</template>
+        <template #body>
+            Are you sure?
+        </template>
+        <template #footer>
+            <button
+              class="modal-default-button button-danger"
+              @click="$emit('confirmed')"
+            >Yes</button>
+            <button
+              class="modal-default-button button-safe"
+              @click="$emit('close')"
+            >No</button>
+        </template>
+        </modal>
+    </Teleport>
 </template>
 <script>
 import filmView from './filmDetails.vue'
@@ -38,19 +60,15 @@ import Modal from './Modal.vue'
         data() {
             return {
                 filmDetailId: 0,
-                currentFilm: null,
-                current: {
-                    title: null,
-                    genre: null,
-                    duration: null,
-                    producer: null,
-                    actors: null,
-                    description: null
-                }
+                filmDeleteId: 0,
+                currentFilm: null
             }
         },
         watch: {
             filmDetailId(newId, oldId) {
+                this.currentFilm = this.films.find((item)=> item.id == newId)
+            },
+            filmDeleteId(newId, oldId) {
                 this.currentFilm = this.films.find((item)=> item.id == newId)
             }
         }
@@ -61,6 +79,12 @@ table,
 th,
 td {
     border: 1px solid black;
+}
+.button-danger{
+    background-color: red;
+}
+.button-safe{
+    background-color: green;
 }
 </style>
 
